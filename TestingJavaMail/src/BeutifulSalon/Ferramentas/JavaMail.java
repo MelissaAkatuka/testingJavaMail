39abeeb06728e1f195e4db669f3b5b763e5d9685
Melissa Akatuka <78980246+MelissaAkatuka@users.noreply.github.com>
2021-08-16 17:32:25 -0300

    add files with error in getPasswordAuthentication()
    added files for sending emails with JavaMail library, error contained in getPasswordAuthentication()

---
+++ TestingJavaMail/src/BeutifulSalon/Ferramentas/JavaMail.java
@@ -0,0 +1,13 @@
+package BeutifulSalon.Ferramentas;
+
+/**
+ *
+ * @author Melissa
+ */
+public class JavaMail {
+    public static void main(String[] args) throws Exception{
+        //JavaMailUtil.sendMail("dev1belosalao@gmail.com");
+        JavaMailUtil.sendMail("melissa.akatsuka@gmail.com");
+    }
+    
+}
---
+++ TestingJavaMail/src/BeutifulSalon/Ferramentas/JavaMailUtil.java
@@ -0,0 +1,63 @@
+package BeutifulSalon.Ferramentas;
+
+import javax.mail.Authenticator;
+import java.net.PasswordAuthentication;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.PasswordAuthenticator;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author Melissa
+ */
+public class JavaMailUtil {
+    public static void sendMail(String recepient) throws Exception {
+        System.out.println("Preparando para enviar email");
+        Properties properties = new Properties();
+        
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        
+        String myAccountEmail = "dev1belosalao@gmail.com";
+        String password = "testeProjBS";
+        
+        Session session = Session.getInstance(properties, new Authenticator(){
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+        
+        Message message = prepareMessage(session, myAccountEmail, recepient);
+        
+        Transport.send(message);
+        System.out.println("Mensagem enviada com sucesso!");
+        
+    }
+
+    private static Message prepareMessage(Session session, String myAccountEmail, String recepient) {
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject("Meu primeiro email");
+            message.setText("Oi, \n Olha meu email!");
+            return message;
+        } catch (Exception ex) {
+            Logger.getLogger(JavaMailUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
+    
+
