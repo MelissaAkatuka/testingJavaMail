65a7d692ec11da80295d5a3a9d533f14dc088de5
David Buzatto <davidbuzatto@gmail.com>
2021-08-23 15:41:56 -0300

    JavaMail 1.6.2
    Inserção e configuração da biblioteca JavaMail 1.6.2

--- TestingJavaMail/src/BeutifulSalon/Ferramentas/JavaMail.java
+++ TestingJavaMail/src/BeutifulSalon/Ferramentas/JavaMail.java
@@ -5,9 +5,9 @@ package BeutifulSalon.Ferramentas;
  * @author Melissa
  */
 public class JavaMail {
-    public static void main(String[] args) throws Exception{
-        //JavaMailUtil.sendMail("dev1belosalao@gmail.com");
-        JavaMailUtil.sendMail("melissa.akatsuka@gmail.com");
+
+    public static void main( String[] args ) throws Exception {
+        JavaMailUtil.sendMail( "melissa.akatsuka@gmail.com" );
     }
-    
+
 }
--- TestingJavaMail/src/BeutifulSalon/Ferramentas/JavaMailUtil.java
+++ TestingJavaMail/src/BeutifulSalon/Ferramentas/JavaMailUtil.java
@@ -16,45 +16,57 @@ import java.util.logging.Logger;
  * @author Melissa
  */
 public class JavaMailUtil {
-    public static void sendMail(String recepient) throws Exception {
-        System.out.println("Preparando para enviar email");
+
+    public static void sendMail( String recepient ) throws Exception {
+
+        System.out.println( "Preparando para enviar email" );
         Properties properties = new Properties();
-        
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "587");
+
+        properties.put( "mail.smtp.auth", "true" );
+        properties.put( "mail.smtp.starttls.enable", "true" );
+        properties.put( "mail.smtp.host", "smtp.gmail.com" );
+        properties.put( "mail.smtp.port", "587" );
 
         String myAccountEmail = "dev1belosalao@gmail.com";
         String password = "testeProjBS";
 
-        Session session = Session.getInstance(properties, new Authenticator(){
+        Session session = Session.getInstance( properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(myAccountEmail, password);
+                return new PasswordAuthentication( myAccountEmail, password );
             }
-        });
-        
-        Message message = prepareMessage(session, myAccountEmail, recepient);
-        
-        Transport.send(message);
-        System.out.println("Mensagem enviada com sucesso!");
-        
+        } );
+
+        Message message = prepareMessage( session, myAccountEmail, recepient );
+
+        Transport.send( message );
+        System.out.println( "Mensagem enviada com sucesso!" );
+
     }
 
-    private static Message prepareMessage(Session session, String myAccountEmail, String recepient) {
+    private static Message prepareMessage(
+            Session session,
+            String myAccountEmail,
+            String recepient ) {
+
         try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(myAccountEmail));
-            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
-            message.setSubject("Meu primeiro email");
-            message.setText("Oi, \n Olha meu email!");
+
+            Message message = new MimeMessage( session );
+            message.setFrom( new InternetAddress( myAccountEmail ) );
+            message.setRecipient(
+                    Message.RecipientType.TO,
+                    new InternetAddress( recepient ) );
+
+            message.setSubject( "Meu primeiro email" );
+            message.setText( "Oi, \n Olha meu email!" );
+
             return message;
-        } catch (Exception ex) {
-            Logger.getLogger(JavaMailUtil.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch ( Exception ex ) {
+            Logger.getLogger( JavaMailUtil.class.getName() ).log( Level.SEVERE, null, ex );
         }
+
         return null;
+
     }
 }
-    
-
